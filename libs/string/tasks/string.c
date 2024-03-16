530d483cd1aba6bff696b4d49e6be9bc9f39e69d
Виталий Курбанов <vitalikhey2017@gmail.com>
2024-03-16 11:06:59 +0300

    Add 6 tasks

--- libs/string/tasks/string.c
+++ libs/string/tasks/string.c
@@ -21,6 +21,7 @@ char* findNonSpace(char *begin) {
     while (*end == ' ' && *end != '\0') {
         end++;
     }
+
     return end;
 }
 
@@ -30,6 +31,7 @@ char* findSpace(char *begin) {
     while (*end != ' ' && *end != '\0') {
         end++;
     }
+
     return end;
 }
 
@@ -37,6 +39,7 @@ char* findNonSpaceReverse(char *rbegin, const char *rend)  {
     while (rbegin != rend && (*rbegin == ' ' || *rbegin == '\0')) {
         rbegin--;
     }
+
     return rbegin;
 }
 
@@ -44,6 +47,7 @@ char* findSpaceReverse(char *rbegin, const char *rend) {
     while (rbegin != rend && (*rbegin != ' ' || *rbegin == '\0')) {
         rbegin--;
     }
+
     return rbegin;
 }
 
@@ -51,7 +55,44 @@ int strcmp(const char *lhs, const char *rhs) {
     for (; *lhs == *rhs; lhs++, rhs++) {
         if (*lhs == '\0') return 0;
     }
+
     return *lhs - *rhs;
+}
+
+char* copy(const char *beginSource, const char *endSource, char *beginDestination) {
+    char *current = beginDestination;
+    while (beginSource != endSource) {
+        *current = *beginSource;
+        current++;
+        beginSource++;
+    }
+    return current;
+}
+
+char* copyIf(char *beginSource, const char *endSource, char *beginDestination, int (*f)(int)) {
+    char *current = beginDestination;
+
+    while (beginSource != endSource) {
+        if (f(*beginSource)) {
+            *current = *beginSource;
+            current++;
+        }
+        beginSource++;
+    }
+
+    return current;
+}
+
+char* copyIfReverse(char *rbeginSource, const char *rendSource, char *beginDestination, int (*f)(int)) {
+    char *current = beginDestination;
+    while (rbeginSource != rendSource) {
+        if (f(*rbeginSource)) {
+            *current = *rbeginSource;
+            current++;
+        }
+        rbeginSource--;
+    }
+    return beginDestination;
 }
 
 
--- libs/string/tasks/string.h
+++ libs/string/tasks/string.h
@@ -13,6 +13,14 @@ char* findSpace(char *begin);
 
 char* findNonSpaceReverse(char *rbegin, const char *rend);
 
+char* findSpaceReverse(char *rbegin, const char *rend);
+
 int strcmp(const char *lhs, const char *rhs);
+
+char* copy(const char *beginSource, const char *endSource, char *beginDestination);
+
+char* copyIf(char *beginSource, const char *endSource, char *beginDestination, int (*f)(int));
+
+char* copyIfReverse(char *rbeginSource, const char *rendSource, char *beginDestination, int (*f)(int));
 
 #endif //LABORATORY_17_STRING_H
